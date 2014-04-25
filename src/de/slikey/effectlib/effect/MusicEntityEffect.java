d5851bc60306ff397145c16954ff3fd7bcbfa87c
Slikey <kevin@schinkenbraten.de>
2014-04-25 17:58:14 +0200

    Colors change now randomly.

--- src/de/slikey/effectlib/effect/MusicEntityEffect.java
+++ src/de/slikey/effectlib/effect/MusicEntityEffect.java
@@ -8,17 +8,17 @@ import de.slikey.effectlib.EffectType;
 import de.slikey.effectlib.util.ParticleEffect;
 
 public class MusicEntityEffect extends EntityEffect {
-	
+
 	/**
 	 * Radials to spawn next note.
 	 */
 	public double radialsPerStep = Math.PI / 8;
-	
+
 	/**
 	 * Radius of circle above head
 	 */
 	public float radius = .4f;
-	
+
 	/**
 	 * Current step. Works as a counter
 	 */
@@ -36,7 +36,7 @@ public class MusicEntityEffect extends EntityEffect {
 		Location location = entity.getLocation();
 		location.add(0, 1.9f, 0);
 		location.add(Math.cos(radialsPerStep * step) * radius, 0, Math.sin(radialsPerStep * step) * radius);
-		ParticleEffect.NOTE.display(location, visibleRange);
+		ParticleEffect.NOTE.display(location, visibleRange, 0, 0, 0, .5f, 1);
 		step++;
 	}
 
