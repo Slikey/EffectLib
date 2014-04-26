0f634c512f15c37752180cc9b899c145432f624a
Slikey <kevin@schinkenbraten.de>
2014-04-26 14:07:01 +0200

    Location is now in the middle center of the text and location.getDirection() influences rotation.

--- src/de/slikey/effectlib/effect/TextLocationEffect.java
+++ src/de/slikey/effectlib/effect/TextLocationEffect.java
@@ -20,34 +20,47 @@ public class TextLocationEffect extends LocationEffect {
 	 * Particle to draw the text
 	 */
 	public ParticleEffect particle = ParticleEffect.FLAME;
-	
+
 	/**
 	 * Text to display
 	 */
 	public String text = "Text";
-	
+
 	/**
 	 * Invert the text
 	 */
 	public boolean invert = false;
-	
+
 	/**
 	 * Each stepX pixel will be shown. Saves packets for lower fontsizes.
 	 */
 	public int stepX = 1;
-	
+
 	/**
 	 * Each stepY pixel will be shown. Saves packets for lower fontsizes.
 	 */
 	public int stepY = 1;
-	
+
 	/**
 	 * Scale the font down
 	 */
 	public float size = (float) 1 / 5;
 
-	protected final StringParser parser;
+	/**
+	 * Set this only to true if you are working with changing text. I'll advice
+	 * the parser to recalculate the BufferedImage every iteration.
+	 * Recommended FALSE
+	 */
+	public boolean realtime = false;
 
+	protected final StringParser parser;
+	protected BufferedImage image = null;
+
+	/**
+	 * 
+	 * @param effectManager
+	 * @param location Location of the point in the middle of the text
+	 */
 	public TextLocationEffect(EffectManager effectManager, Location location) {
 		this(effectManager, location, new Font("Tahoma", Font.PLAIN, 16));
 	}
@@ -63,7 +76,8 @@ public class TextLocationEffect extends LocationEffect {
 	@Override
 	public void onRun() {
 		int clr = 0;
-		BufferedImage image = parser.stringToBufferedImage(text);
+		if (image == null || realtime)
+			image = parser.stringToBufferedImage(text);
 		for (int y = 0; y < image.getHeight(); y += stepY) {
 			for (int x = 0; x < image.getWidth(); x += stepX) {
 				clr = image.getRGB(x, y);
@@ -71,8 +85,8 @@ public class TextLocationEffect extends LocationEffect {
 					continue;
 				else if (invert && Color.black.getRGB() == clr)
 					continue;
-				Vector v = new Vector(x, image.getHeight() - y, 0).multiply(size);
-				VectorUtils.rotateAroundAxisY(v, location.getYaw() * MathUtils.degreesToRadians);
+				Vector v = new Vector((float) image.getWidth() / 2 - x, (float) image.getHeight() / 2 - y, 0).multiply(size);
+				VectorUtils.rotateAroundAxisY(v, -location.getYaw() * MathUtils.degreesToRadians);
 				particle.display(location.add(v), visibleRange);
 				location.subtract(v);
 			}
