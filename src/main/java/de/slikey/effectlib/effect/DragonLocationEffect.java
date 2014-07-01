19a7e7b38c82ebff3f76c77470dba584fb2a77c5
Slikey <kevin@schinkenbratenl.de>
2014-07-01 19:03:11 +0200

    Working on 2 new Effects

--- src/main/java/de/slikey/effectlib/effect/DragonLocationEffect.java
+++ src/main/java/de/slikey/effectlib/effect/DragonLocationEffect.java
@@ -9,6 +9,9 @@ import de.slikey.effectlib.util.VectorUtils;
 import org.bukkit.Location;
 import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DragonLocationEffect extends LocationEffect {
 
     /**
@@ -41,20 +44,27 @@ public class DragonLocationEffect extends LocationEffect {
      */
     protected int step = 0;
 
+    protected final List<Float> rndF, rndAngle;
+
     public DragonLocationEffect(EffectManager effectManager, Location location) {
         super(effectManager, location);
         type = EffectType.REPEATING;
         period = 5;
         iterations = 200;
+        rndF = new ArrayList<Float>(arcs);
+        rndAngle = new ArrayList<Float>(arcs);
     }
 
     @Override
     public void onRun() {
+        while (rndF.size() < arcs) rndF.add(RandomUtils.random.nextFloat());
+        while (rndAngle.size() < arcs) rndAngle.add(RandomUtils.random.nextFloat());
         for (int i = 0; i < arcs; i++) {
-            float pitch = 1 + RandomUtils.random.nextFloat() * 2 * this.pitch - this.pitch;
+            float pitch = 1 + rndF.get(i) * 2 * this.pitch - this.pitch;
             float x = step % particles;
             float y = (float) (pitch * Math.pow(step, 2));
             Vector v = new Vector(x * length / particles, y, 0);
+            VectorUtils.rotateAroundAxisX(v, rndAngle.get(i));
             VectorUtils.rotateAroundAxisZ(v, location.getPitch() * MathUtils.degreesToRadians);
             VectorUtils.rotateAroundAxisY(v, -location.getYaw() * MathUtils.degreesToRadians);
             particle.display(location.add(v), visibleRange, 0, 0, 0, 0, 1);
