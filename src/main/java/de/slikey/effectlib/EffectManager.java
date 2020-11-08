5edb2c94d00f205dc98441e902b48577a82758ac
Nathan Wolf <nathan@elmakers.com>
2020-11-07 16:09:08 -0800

    Fix warnings when using particle_offset shortcuts

--- src/main/java/de/slikey/effectlib/EffectManager.java
+++ src/main/java/de/slikey/effectlib/EffectManager.java
@@ -221,11 +221,13 @@ public class EffectManager implements Disposable {
             parameters.set("particle_offset_x", parameters.get("particle_offset"));
             parameters.set("particle_offset_y", parameters.get("particle_offset"));
             parameters.set("particle_offset_z", parameters.get("particle_offset"));
+            parameters.set("particle_offset", null);
         }
         if (parameters.contains("particleOffset")) {
             parameters.set("particleOffsetX", parameters.get("particleOffset"));
             parameters.set("particleOffsetY", parameters.get("particleOffset"));
             parameters.set("particleOffsetZ", parameters.get("particleOffset"));
+            parameters.set("particleOffset", null);
         }
 
         Collection<String> keys = parameters.getKeys(false);
